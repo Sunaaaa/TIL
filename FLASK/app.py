e645e9d8107e2bc3064618610fee92d34ff5ffa4
Sunah Gong <glglthssla@naver.com>
2019-10-23 10:59:41 +0900

    FLask ( app.py / index.html / greeting.html / cube.html )

---
+++ FLASK/app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    # return "Hello World!"
+    return render_template('index.html')
+    
+@app.route('/dohyeon')
+def dohyeon():
+    return "저는 무술가 입니다."
+
+@app.route('/html')
+def html():
+    return "<h1>태그도 사용할 수 있어요</h1>"
+
+@app.route('/html_multiline')
+def html_multiline():
+    return '''
+    <ol>
+        <li>안녕</li>
+        <li>안녕2</li>
+    </ol>
+
+    '''
+
+# 동적 라우팅 (Variable Routing)
+@app.route('/greeting/<string:name>')
+def greeting(name):
+    # return f'안녕~, {name}??'
+    return render_template('greeting.html', html_name=name)
+
+# 세제곱을 돌려주는 cube 페이지 작성
+# 사용자한테 숫자값을 받아, 세제곱한 결과를 보여주는 페이지
+@app.route('/cube/<string:num>')
+def cube(num):
+    a = int(num)
+    b = a*a*a
+    return render_template('cube.html', result=b)
+
+@app.route('/cube2/<int:num>')
+def cube2(num):
+    return render_template('cube.html', result=num**3)
+
+
+# end of file !!!!!
+# debug 모드를 활성화해서 서버 새로고침을 생략한다.
+if __name__ == '__main__':
+    app.run(debug=True)
