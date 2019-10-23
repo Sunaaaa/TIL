6a955d0c908ffd1181a7176285ab2033a8e1ce6c
Sunah Gong <glglthssla@naver.com>
2019-10-23 09:44:33 +0900

    python 연습문제 5개 ( p1 / p2 / p3 / p4 / p5 )

---
+++ Python/p1.py
@@ -0,0 +1,11 @@
+# 문제 1.
+# 문자열을 입력받아 문자열의 첫 글자와 마지막 글자를 출력하는 프로그램을 작성하시오.
+
+my_str = input('문자를 입력하세요: ')
+
+print(my_str[0], my_str[-1])
+print(my_str[0], my_str[len(str)-1])
+
+
+# 문자를 입력하세요: python
+# p n
---
+++ Python/p2.py
@@ -0,0 +1,14 @@
+# 문제 2.
+# 자연수 N이 주어졌을 때, 1부터 N까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+
+numbers = int(input('숫자를 입력하세요: '))
+
+for number in range(1, numbers+1):
+    print(number)
+
+# 숫자를 입력하세요: 5
+# 1
+# 2
+# 3
+# 4
+# 5
