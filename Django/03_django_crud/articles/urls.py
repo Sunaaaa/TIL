481fb1fb17f8bae46b21fe10674085acf19fb21f
Sunah Gong <glglthssla@naver.com>
2019-10-31 13:04:32 +0900

    Django | ( urls.py / views.py / settings.py / create.html / detail.html / edit.html / index.html / new.html )

--- Django/03_django_crud/articles/urls.py
+++ Django/03_django_crud/articles/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name='articles'
 urlpatterns = [
-    path('<int:article_pk>/', views.detail),
-    path('new/', views.new),
-    path('create/', views.create),
-    path('', views.index),
+    path('<int:article_pk>/update/', views.update, name='update'), # UODATE Logic - 폼 전달
+    path('<int:article_pk>/edit/', views.edit, name='edit'), # UODATE Logic - 폼 전달
+    path('<int:article_pk>/delete/', views.delete, name='delete'), # DELETE Logic 
+    path('<int:article_pk>/', views.detail, name='detail'), # READ Logic - Detail
+    path('new/', views.new, name='new'), # CREATE Logic - 데이터 베이스에 저장
+    path('create/', views.create, name='create'), # CREATE Logic - 사용자에게 폼 전달
+    path('', views.index, name='index'), # READ Logic - Index
 
 ]
--- Django/03_django_crud/articles/views.py
+++ Django/03_django_crud/articles/views.py
@@ -3,7 +3,7 @@ from .models import Article
 
 # Create your views here.
 def index(request):
-    articles = Article.objects.all()[:]
+    articles = Article.objects.all()[::-1]
     context = {
         'articles' : articles
     }
@@ -18,13 +18,18 @@ def new(request):
 
 # 사용자로부터 데이터를 받아서 DB에 저장하는 함수
 def create(request):
+
+    # POST 방식으로 사용자가 입력한 데이터를 받는다.
     title = request.POST.get('title')
     content = request.POST.get('content')
 
+    # 인스턴스를 생성하여 데이터 베이스에 저장한다. 
     article = Article(title=title, content=content)
     article.save()
+   
+    article_pk = article.pk
 
-    return redirect('/articles/')
+    return redirect(f'/articles/{article_pk}')
 
 
 # 게시글 상세 정보를 가져오는 함수
@@ -36,4 +41,36 @@ def detail(request, article_pk):
     return render(request, 'articles/detail.html', context)
 
 
-# 
+# 게시글 삭제
+def delete(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    article.delete()
+    return redirect('/articles/')
+
+
+# 게시글 수정
+def edit(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    context = {
+        'article' : article
+    }
+
+    return render(request, 'articles/edit.html', context)
+
+# 수정된 내용을 전달 받아서 DB에 저장 (반영)
+def update(request, article_pk):
+
+    # 1. 수정할 게시글 인스턴스 가져오기
+    article = Article.objects.get(pk=article_pk)
+
+    # 2. 폼에서 전달받은 데이터 덮어쓰기
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+    article.title = title
+    article.content = content
+    
+    # 3. DB 저장
+    article.save()
+
+    # 3. DB 저장 완료 후 게시글 detail로 이동
+    return redirect(f'/articles/{article_pk}')
