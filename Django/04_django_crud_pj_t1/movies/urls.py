a89b3542dce0a5cb4d77f58efb96fca26cb5ce31
Sunah Gong <glglthssla@naver.com>
2019-11-01 11:41:59 +0900

    CRUD 기본 기능 구현 완료!

--- Django/04_django_crud_pj_t1/movies/urls.py
+++ Django/04_django_crud_pj_t1/movies/urls.py
@@ -3,6 +3,12 @@ from . import views
 
 app_name='movies'
 urlpatterns = [
+    path('new/', views.new, name='new'),
+    path('create/', views.create, name='create'),
+    path('<int:movie_pk>/', views.detail, name='detail'),
+    path('<int:movie_pk>/edit', views.edit, name='edit'),
+    path('<int:movie_pk>/update', views.update, name='update'),
+    path('<int:movie_pk>/delete', views.delete, name='delete'),
     path('', views.index, name='index'), # READ Logic - Index
 
 ]
--- Django/04_django_crud_pj_t1/movies/views.py
+++ Django/04_django_crud_pj_t1/movies/views.py
@@ -1,5 +1,75 @@
 from django.shortcuts import render, redirect
+from .models import Movie
 
 # Create your views here.
 def index(request):
-    return render(request, 'movies/index.html')
+
+    movies = Movie.objects.all()
+    context = {
+        'movies' : movies
+    }
+
+    return render(request, 'movies/index.html', context)
+
+def new(request):
+    return render(request, 'movies/new.html')
+
+def create(request):
+    movie = Movie()
+    movie.title = request.POST.get('title')
+    movie.title_en = request.POST.get('title_en')
+    movie.audience = request.POST.get('audience')
+    movie.open_date = request.POST.get('open_date')
+    movie.genre = request.POST.get('genre')
+    movie.watch_grade = request.POST.get('watch_grade')
+    movie.score = request.POST.get('score')
+    movie.poster_url = request.POST.get('poster_url')
+    movie.description = request.POST.get('description')
+
+    movie.save()
+
+    return redirect('movies:detail', movie.pk)
+    
+
+def detail(request, movie_pk):
+    movie = Movie.objects.get(pk=movie_pk)
+    context = {
+        'movie' : movie
+    }
+
+    return render(request, 'movies/detail.html', context)
+
+def edit(request, movie_pk):
+    movie = Movie.objects.get(pk=movie_pk)
+    context = {
+        'movie' : movie
+    }
+
+    return render(request, 'movies/edit.html', context)
+
+def update(request, movie_pk):
+    movie = Movie.objects.get(pk=movie_pk)
+
+    movie.title = request.POST.get('title')
+    movie.title_en = request.POST.get('title_en')
+    movie.audience = request.POST.get('audience')
+    movie.open_date = request.POST.get('open_date')
+    movie.genre = request.POST.get('genre')
+    movie.watch_grade = request.POST.get('watch_grade')
+    movie.score = request.POST.get('score')
+    movie.poster_url = request.POST.get('poster_url')
+    movie.description = request.POST.get('description')
+
+    movie.save()
+
+    context = {
+        'movie' : movie
+    }
+    return redirect('movies:detail', movie.pk)
+
+def delete(request, movie_pk):
+    movie = Movie.objects.get(pk=movie_pk)
+    movie.delete()
+
+    return redirect('movies:index')
+
