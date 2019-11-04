e8d808e964572dc32f44e42bdadcf1a6ef255f6b
Sunah Gong <glglthssla@naver.com>
2019-11-04 13:52:48 +0900

    Seed Data

--- Django/04_django_crud_pj_t1/movies/models.py
+++ Django/04_django_crud_pj_t1/movies/models.py
@@ -5,7 +5,7 @@ class Movie(models.Model):
     title = models.CharField(max_length=50)
     title_en = models.CharField(max_length=60)
     audience = models.IntegerField()
-    open_date = models.DateField()
+    open_date = models.CharField(max_length=50)
     genre = models.CharField(max_length=50)
     watch_grade = models.CharField(max_length=10)
     score = models.FloatField()
