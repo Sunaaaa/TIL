d0a50c683ef0c3e18aaaf972aac180ecbd8eda96
Sunah Gong <glglthssla@naver.com>
2019-11-11 14:19:22 +0900

    회원정보수정 | 비밀번호 변경 |   django Form 하나의 html로 합치기

--- Django/05_django_form/acounts/views.py
+++ Django/05_django_form/acounts/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.views.decorators.http import require_POST
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserChangeForm
 # Create your views here.
 # Authentication (인증) -> 신원 확인
 # - 자신이 누구라고 주장하는 사람의 신원을 확인하는 것
@@ -67,3 +70,43 @@ def delete(request):
     request.user.delete()
     # 지금 접속하고 있는 user 삭제
     return redirect('articles:index')
+
+
+# 회원정보 수정
+@login_required
+def update(request):
+    if request.method == "POST":
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+        
+    else:
+        # 회원정보 폼에는 user의 정보가 들어가 있어야 한다.
+        # instance로 request.user를 통해 사용자의 정보를 가져온다.
+        form = CustomUserChangeForm(instance=request.user)
+    
+    context = {
+        'form' : form
+    }
+
+    return render(request, 'acounts/update.html', context)
+
+# 비밀번호 수정
+def change_password(request):
+    if request.method == "POST":
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            return redirect('articles:index')
+        pass
+
+    else:
+        form = PasswordChangeForm(request.user)
+
+    context = {
+        'form' : form
+    }
+
+    return render(request, 'acounts/change_password.html', context)
