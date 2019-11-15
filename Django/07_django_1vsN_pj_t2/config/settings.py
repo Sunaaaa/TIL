8bd0e9be51e41e134d7f2e0b860273f135a8748a
Sunah Gong <glglthssla@naver.com>
2019-11-15 10:07:05 +0900

    HPHK 프로젝트 2 Model 생성 완료  !

--- Django/07_django_1vsN_pj_t2/config/settings.py
+++ Django/07_django_1vsN_pj_t2/config/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'movies',
     'bootstrap4',
+    'imagekit',
     'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
--- Django/07_django_1vsN_pj_t2/movies/models.py
+++ Django/07_django_1vsN_pj_t2/movies/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from imagekit.models import ProcessedImageField
+from imagekit.processors import Thumbnail
+from django.conf import settings
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+
+    poster = ProcessedImageField(
+        processors=[Thumbnail(200,300)],    # 처리할 작업
+        format='JPEG',              # 이미지 포맷
+        options={                   # 각종 추가 옵션
+            'quality' : 90
+        },
+        upload_to = 'movies/image' # 저장 위치
+        # 실제 경로 : MEDIA_ROOT/movies/image
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title} : {self.user}'
+
+class Rating(models.Model):
+    score = models.FloatField()
+    content = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user}의 평 : {self.score} 점 / {self.content}'
