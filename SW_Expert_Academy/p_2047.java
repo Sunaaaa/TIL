97a16253356c296e489dce68632a14a7fdf49bdc
Sunaaaa <39547788+Sunaaaa@users.noreply.github.com>
2020-01-12 03:55:04 +0900

    2047

---
+++ SW_Expert_Academy/p_2047.java
@@ -0,0 +1,14 @@
+// package lv_1;
+/*
+    SW Expert Academy 2047. 신문 헤드라인
+    문제 : https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QKsLaAy0DFAUq&categoryId=AV5QKsLaAy0DFAUq&categoryType=CODE
+*/
+import java.util.Scanner;
+
+public class p_2047 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String data = sc.next();
+		System.out.println(data.toUpperCase());
+	}
+}
