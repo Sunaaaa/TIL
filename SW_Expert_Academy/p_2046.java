4c3ee98af129b469813786a787adccd4907fb210
Sunaaaa <39547788+Sunaaaa@users.noreply.github.com>
2020-01-12 03:59:14 +0900

    2046

---
+++ SW_Expert_Academy/p_2046.java
@@ -0,0 +1,16 @@
+// package lv_1;
+/*
+    SW Expert Academy 2046. 스탬프 찍기
+    문제 : https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QKdT6AyYDFAUq&categoryId=AV5QKdT6AyYDFAUq&categoryType=CODE
+*/
+import java.util.Scanner;
+
+public class p_2046 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int count = sc.nextInt();
+		for (int i = 0; i < count; i++) {
+			System.out.print("#");
+		}
+	}
+}
