87fe62ceb4a7746d27da40c8567e0e183a8bb679
Sunaaaa <39547788+Sunaaaa@users.noreply.github.com>
2020-01-12 02:21:35 +0900

    2071

---
+++ SW_Expert_Academy/p_2071.java
@@ -0,0 +1,28 @@
+// package lv_1;
+/*
+    SW Expert Academy 2071. 평균값 구하기
+    문제 : https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QRnJqA5cDFAUq&categoryId=AV5QRnJqA5cDFAUq&categoryType=CODE
+*/
+
+import java.util.Scanner;
+
+public class p_2071 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int count = 0;
+		float sum;
+		int test = sc.nextInt();
+		int avg_array[] = new int[test];
+		while(count < test) {
+			sum = 0.0f;
+			for (int i = 0; i < 10; i++) {
+				sum+=sc.nextInt();				
+			}
+			avg_array[count] = Math.round(sum/10);
+			count++;				
+		}
+		for (int i = 0; i < avg_array.length; i++) {
+			System.out.println("#"+i+" "+avg_array[i]);
+		}
+	}
+}
