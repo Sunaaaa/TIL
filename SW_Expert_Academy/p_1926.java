74f9abefdc51a9da41debc9c57d2e221f9752b3c
Sunaaaa <39547788+Sunaaaa@users.noreply.github.com>
2020-01-13 01:14:52 +0900

    1926[진행중]

---
+++ SW_Expert_Academy/p_1926.java
@@ -0,0 +1,6 @@
+// package lv_2;
+/*
+    SW Expert Academy 1926. 간단한 369게임
+    문제 : https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PTeo6AHUDFAUq&categoryId=AV5PTeo6AHUDFAUq&categoryType=CODE&&&
+*/
+
